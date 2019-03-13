d52beb925d2f10b06976bc5ccdc9762c1a4d165b
AnjuKukreja <48277159+anjukukreja@users.noreply.github.com>
2019-03-13 16:27:52 +0530

    Adding logic to unzip file & a method to test the same

--- main/inputoutput/io.py
+++ main/inputoutput/io.py
@@ -1,2 +1,9 @@
+from zipfile import ZipFile
 def inc(num):
     return num+2
+
+def unzip_file(fromPath,toPath):
+    file=ZipFile(fromPath,'r')
+    file.extractall(toPath)
+    file.close()
+    return 1
--- test/inputoutput/test_io.py
+++ test/inputoutput/test_io.py
@@ -1,6 +1,13 @@
-from main.inputoutput.io import inc
+from main.inputoutput.io import inc,unzip_file
+import os
+
 def test_answer():
     assert inc(3) == 5
 
-def printMe():
-    print ("hi")
+def test_unzip_file():
+    fromPath=r"test\data\Desktop.zip"
+    toPath=r"test\data\target"
+    curDir=os.getcwd()
+    fromPath=os.path.join(str(curDir),str(fromPath))
+    toPath=os.path.join(str(curDir),str(toPath))
+    assert unzip_file(fromPath,toPath) == 1
